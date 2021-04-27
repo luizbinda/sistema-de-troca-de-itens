ee3b2158f516c678aec6fcf014f24b78dfd30ad7
Herbert <herbertpansini@hotmail.com>
2021-04-27 18:34:11 -0300

    first commit

---
+++ src/test/java/com/colatina/sti/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colatina.sti.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
