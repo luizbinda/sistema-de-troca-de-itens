7f49aea681d80b3a4b57fafffdd8b577c57b07f8
jortolani <jhordan.orto@gmail.com>
2021-04-28 20:21:43 -0300

    fix - conflitos

---
+++ src/main/java/com/colatina/sti/service/domain/User.java
@@ -0,0 +1,26 @@
+package com.colatina.sti.service.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
+    @SequenceGenerator(name = "seq_user", allocationSize = 1, sequenceName = "seq_user")
+    @Column(name = "id")
+    private Long id;
+
+
+
+    @Column(name = "dt_nascimento")
+    private LocalDate dataNascimento;
+
+}
