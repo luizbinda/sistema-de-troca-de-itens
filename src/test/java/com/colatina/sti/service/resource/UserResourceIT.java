188fef80bc986e3c53b11a7466af9c9945c03f31
luizbinda <luiznandopimenta52@gmail.com>
2021-05-08 00:45:27 -0300

    feat - configuração h2 finalizada

--- src/test/java/com/colatina/sti/service/resource/UserResourceIT.java
+++ src/test/java/com/colatina/sti/service/resource/UserResourceIT.java
@@ -1,20 +1,16 @@
 package com.colatina.sti.service.resource;
 
+import com.colatina.sti.service.ServiceApplication;
 import com.colatina.sti.service.builder.UserBuilder;
 import com.colatina.sti.service.domain.User;
-import com.colatina.sti.service.service.UserService;
 import com.colatina.sti.service.service.mapper.UserMapper;
-import com.colatina.sti.service.util.H2JpaConfig;
 import com.colatina.sti.service.util.IntTestComum;
 import com.colatina.sti.service.util.TestUtil;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import static org.hamcrest.Matchers.hasSize;
@@ -24,28 +20,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(
-        classes = { H2JpaConfig.class },
-        loader = AnnotationConfigContextLoader.class
-)
 @Transactional
+@SpringBootTest(classes = ServiceApplication.class)
 public class UserResourceIT extends IntTestComum {
 
   @Autowired
   private UserBuilder builder;
 
   @Autowired
-  private UserService userService;
-
-  @Autowired
   private UserMapper userMapper;
 
   private final String URL = "/api/users";
 
-//  @BeforeEach
-//  public void reset() {
-//
-//  }
 
   @Test
   public void listagem() throws Exception {
@@ -54,7 +40,7 @@ public class UserResourceIT extends IntTestComum {
     mockaaa.perform(get(URL)
             .contentType(TestUtil.APPLICATION_JSON_UTF8))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$",hasSize(2)));
+            .andExpect(jsonPath("$",hasSize(1)));
   }
 
   @Test
